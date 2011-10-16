d045457e2b09ca3eae94fe1903bc9ef33a68ad01
Chinmay Kale <Chinmay.V.Kale@gmail.com>
2011-10-16 11:40:41 -0400

    added

---
+++ src/edu/uga/cs/restendpoint/utils/OntModelWrapper.java
@@ -0,0 +1,34 @@
+package edu.uga.cs.restendpoint.utils;
+
+import com.hp.hpl.jena.ontology.OntModel;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: kale
+ * Date: 10/16/11
+ * Time: 12:18 AM
+ * Email: <kale@cs.uga.edu>
+ */
+public class OntModelWrapper {
+    private final OntModel ontModel;
+    private final String URI;
+    private final String ontologyName;
+
+    public OntModelWrapper(OntModel ontModel, String URI, String ontologyName) {
+        this.ontModel = ontModel;
+        this.URI = URI;
+        this.ontologyName = ontologyName;
+    }
+
+    public OntModel getOntModel() {
+        return ontModel;
+    }
+
+    public String getURI() {
+        return URI;
+    }
+
+    public String getOntologyName() {
+        return ontologyName;
+    }
+}
