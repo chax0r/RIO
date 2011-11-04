f2b8ec707b5eb14cb677394acda2bb7cbe0d9562
Chinmay Kale <Chinmay.V.Kale@gmail.com>
2011-11-03 21:39:09 -0400

    modified

--- src/edu/uga/cs/restendpoint/model/SparqlQueryResultStore.java
+++ src/edu/uga/cs/restendpoint/model/SparqlQueryResultStore.java
@@ -2,17 +2,41 @@ package edu.uga.cs.restendpoint.model;
 
 import com.hp.hpl.jena.query.ResultSet;
 import com.hp.hpl.jena.shared.PrefixMapping;
+import com.hp.hpl.jena.sparql.core.Prologue;
+import org.joda.time.DateTime;
 
 /**
- * Created by IntelliJ IDEA.
- * User: kale
+ * Author: kale
  * Date: 10/18/11
  * Time: 4:49 AM
  * Email: <kale@cs.uga.edu>
  */
 public class SparqlQueryResultStore {
-    private Integer id;
+    private String id;
     private ResultSet resultSet;
-    private PrefixMapping prefixMap;
-    private long timeout;
+    private Prologue prologue;
+    private DateTime createTime;
+
+    public SparqlQueryResultStore(String id, ResultSet resultSet, Prologue prologue, DateTime createTime ) {
+        this.id = id;
+        this.resultSet = resultSet;
+        this.prologue = prologue;
+        this.createTime = createTime;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public ResultSet getResultSet() {
+        return resultSet;
+    }
+
+    public Prologue getPrologue() {
+        return prologue;
+    }
+
+    public DateTime getCreateTime() {
+        return createTime;
+    }
 }
