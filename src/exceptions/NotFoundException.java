a01e309ae3ee02d2f9000df892665f6d1c357b7c
Chinmay Kale <Chinmay.V.Kale@gmail.com>
2011-11-04 00:05:29 -0400

    added

---
+++ src/exceptions/NotFoundException.java
@@ -0,0 +1,16 @@
+package exceptions;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: kale
+ * Date: 11/3/11
+ * Time: 11:47 PM
+ * Email: <kale@cs.uga.edu>
+ */
+public class NotFoundException extends RuntimeException
+{
+   public NotFoundException(String s)
+   {
+      super(s);
+   }
+}
---
+++ src/exceptions/mappers/NotFoundExceptionMapper.java
@@ -0,0 +1,24 @@
+package exceptions.mappers;
+
+import exceptions.NotFoundException;
+
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+/**
+ * Author: kale
+ * Date: 11/3/11
+ * Time: 11:45 PM
+ * Email: <kale@cs.uga.edu>
+ */
+@Provider
+public class NotFoundExceptionMapper implements ExceptionMapper<NotFoundException>
+{
+   public Response toResponse(NotFoundException exception)
+   {
+      return Response.status(Response.Status.NOT_FOUND)
+              .entity(exception.getMessage())
+              .type("text/plain").build();
+   }
+}
